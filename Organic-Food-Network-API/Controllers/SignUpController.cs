9186c2907a34e7451f565ae6a8cddc3dce160a91
EliasHaloun0104 <37835421+EliasHaloun0104@users.noreply.github.com>
2020-10-06 10:57:25 +0200

    Update Configuration & Generate API

---
+++ Organic-Food-Network-API/Controllers/SignUpController.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using Organic_Food_Network_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Organic_Food_Network_API.Controllers
+{
+    public class SignUpController : ApiController
+    {
+        private Entities db = new Entities();
+
+        // POST: api/SignUp
+        [ResponseType(typeof(Person))]
+        public async Task<IHttpActionResult> PostPerson(Person person)
+        {
+            Debug.WriteLine("HelloWorld!");
+            var json = JsonConvert.SerializeObject(person);
+            Debug.WriteLine(json.ToString());
+
+            
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (NameExist(person.Name))
+            {
+                return Ok("Name already Exist");
+            }
+
+            db.People.Add(person);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = person.Id }, person);
+        }
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+        private bool NameExist(string name)
+        {
+            return db.People.Count(e => (e.Name == name)) > 0;
+        }
+    }
+}
